6e54791f15e8e9305217a26a1ed43b8cc8de55de
Ajani Maragh <amsbstart@gmail.com>
2024-06-02 19:31:45 -0700

    combined planets and people

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,7 +7,36 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [characters, setCharacters] = useState([])
+ 
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    Promise.all([
+      axios.get('http://localhost:9009/api/people'),
+      axios.get('http://localhost:9009/api/planets')
+    ])
+    .then(([peopleRes, planetRes])=> {
+      const peopleData = peopleRes.data
+      const planetData = planetRes.data
+
+      console.log(peopleData)
+      console.log(planetData)
+
+      const combinedData = peopleData.map(person => {
+        const homeworld = planetData.find(planet => planet.id === person.homeworld);
+        return {
+          ...person,
+          homeworld: homeworld ? { id: homeworld.id, name: `${homeworld.firstName} ${homeworld.lastName}` }
+          : { id: person.homeworld, name: 'unknown planet' }
+        }
+      })
+      setCharacters(combinedData)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+  }, [])
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
